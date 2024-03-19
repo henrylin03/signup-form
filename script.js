a68bc50e07973e1731accc5a55abe80eb804c499
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-03-19 17:02:09 +1100

    use password fields instead of differently selecting password and confirm password fields

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const form = document.querySelector("form");
-const inputs = document.querySelectorAll("input");
-const passwordField = document.querySelector("#password");
-const confirmPasswordField = document.querySelector("#password_confirm");
+const passwordFields = form.querySelectorAll("input[id^='password']");
 
 form.noValidate = true;
 
@@ -10,9 +8,9 @@ function validatePasswords() {
     "li[class^='password']"
   );
   if (
-    passwordField.value &&
-    confirmPasswordField.value &&
-    passwordField.value === confirmPasswordField.value
+    passwordFields[0].value &&
+    passwordFields[1].value &&
+    passwordFields[0].value === passwordFields[1].value
   ) {
     Array.from(passwordInputContainers).forEach((containerElem) => {
       const inputFields = containerElem.children;
@@ -28,66 +26,33 @@ function validatePasswords() {
   }
 }
 
-function validateNonPasswordFields() {
-  const nonPasswordContainers = form.querySelectorAll(
-    "li:not([class^='password'])"
-  );
-  Array.from(nonPasswordContainers).forEach((containerElem) => {
-    const inputElement = containerElem.querySelector("input");
-    if (inputElement.checkValidity()) {
-      Array.from(containerElem.children).forEach((field) =>
-        field.classList.remove("error")
-      );
-    } else {
-      Array.from(containerElem.children).forEach((field) =>
-        field.classList.add("error")
-      );
-    }
-  });
+// function validateNonPasswordFields() {
+//   const nonPasswordContainers = form.querySelectorAll(
+//     "li:not([class^='password'])"
+//   );
+//   Array.from(nonPasswordContainers).forEach((containerElem) => {
+//     const inputElement = containerElem.querySelector("input");
+//     if (inputElement.checkValidity()) {
+//       Array.from(containerElem.children).forEach((field) =>
+//         field.classList.remove("error")
+//       );
+//     } else {
+//       Array.from(containerElem.children).forEach((field) =>
+//         field.classList.add("error")
+//       );
+//     }
+//   });
 
-  return;
-}
+//   return;
+// }
 
 function validateForm(e) {
   if (form.checkValidity()) return;
 
   e.preventDefault();
   validatePasswords();
-  validateNonPasswordFields();
+  // validateNonPasswordFields();
 }
 
-// function validatePasswordsMatch() {
-//   if (passwordField.value && confirmPasswordField.value)
-//     return passwordField.value === confirmPasswordField.value;
-//   return false;
-// }
-
-// function validateForm(e) {
-//   if (form.checkValidity()) return;
-
-//   e.preventDefault();
-//   Array.from(inputs).forEach((input) => {
-//     const inputIsValid = (inputElementId) => {
-//       if (inputElementId.includes("password")) validatePasswordsMatch();
-//       else inputElementId.checkValidity;
-//     };
-
-//     let inputId = input.getAttribute("id");
-//     let fieldContainer = form.querySelector(`.${inputId}_container`);
-
-//     if (inputIsValid(inputId)) {
-//       Array.from(fieldContainer.children).forEach((elem) =>
-//         elem.classList.remove("error")
-//       );
-//       return;
-//     }
-
-//     Array.from(fieldContainer.children).forEach((elem) =>
-//       elem.classList.add("error")
-//     );
-//   });
-// }
-
 form.addEventListener("submit", validateForm);
-passwordField.addEventListener("input", validatePasswords);
-confirmPasswordField.addEventListener("input", validatePasswords);
+passwordFields.forEach((f) => f.addEventListener("input", validatePasswords));
